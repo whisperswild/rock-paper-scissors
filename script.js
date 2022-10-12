79bca31f6b494f0d6b0eb12c58cbe8fbf4634633
Chris <Whisperswild@gmail.com>
2022-10-11 21:28:54 -0500

    Create the playing functions and fill in the logic

--- script.js
+++ script.js
@@ -5,19 +5,33 @@ function getComputerChoice(){
 
     switch (computerChoice){
         case: 1
-            computerChoice = "Rock";
+            computerChoice = "rock";
             return computerChoice;
         case: 2
-            computerChoice = "Paper";
+            computerChoice = "paper";
             return computerChoice;
         case: 3
-            computerChoice = "Scissors";
+            computerChoice = "scissors";
             return computerChoice;
     }
 }
 
 function play(playerSelection, computerSelection = getComputerChoice()){{
-    
+    let playerChoice = prompt("What is your choice? Rock, Paper, or Scissors?");
+
+    if(typeof playerChoice === 'string'){
+        playerChoice = playerChoice.toLowerCase();
+        
+        if (playerChoice != "rock" || playerChoice != "paper" || playerChoice != "scissors"){
+            alert("You must enter a valid option!")
+            playerChoice = prompt("What is your choice? Rock, Paper, or Scissors?");
+            play(playerSelection, computerSelection = getComputerChoice());
+        } else{
+            getWinner(playerSelection, computerSelection);
+        }
+    }else {
+        
+    }
 
 }}
 
@@ -25,7 +39,31 @@ function game(){
     //play 5 rounds 
 
     for(let i = 0, i<5, i++){
-        let playerChoice = prompt("What is your choice? Rock, Paper, or Scissors?");
+
         play(playerSelection(), computerSelection());
+    }
+}
+
+function getWinner(playerSelection, computerSelection){
+    switch(computerChoice){
+        case: "rock"
+        if (playerChoice === "paper"){
+            getWinner = "Player Wins with Paper!";
+            return;
+        }else if (playerChoice === "rock"){
+            getWinner = "Tie! Try again!";
+        }else{
+            getWinner = "Computer wins with Rock!"
+        }
+
+        case: "paper"
+        if (playerChoice === "paper"){
+            getWinner = "Tie! Try again!";
+            return;
+        }else if (playerChoice === "rock"){
+            getWinner = "Computer wins with Paper!";
+        }else{
+            getWinner = "Player wins with Scissors!"
+        }
     }
 }
